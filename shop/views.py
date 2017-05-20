ce3f3b017c2694a6ac7665c0c248d5d2f2ce094d
EMICSIP <michal.sipa@ericsson.com>
2017-05-20 19:16:10 +0200

    Added: - session based cart with products

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from cart.forms import CartAddProductForm
 
 
 # Create your views here.
@@ -28,9 +29,11 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
 
     context = {
-        'product': product
+        'product': product,
+        'cart_product_form': cart_product_form,
     }
 
     return render(request,
